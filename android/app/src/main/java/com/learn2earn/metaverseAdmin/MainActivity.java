cb158ac39883ce5c833e5ab213697dddb0183687
Zagham Nadeem <zagham@learn2earnn.com>
2023-01-25 18:01:44 +0500

    files added

---
+++ android/app/src/main/java/com/learn2earn/metaverseAdmin/MainActivity.java
@@ -0,0 +1,5 @@
+package com.learn2earn.metaverseAdmin;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/2680.be8d70e5f564a271.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[2680],{2680:(r,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>t});var s=e(5861),a=e(7423);class t extends a.Uw{show(_){return(0,s.Z)(function*(){})()}hide(_){return(0,s.Z)(function*(){})()}}}}]);
